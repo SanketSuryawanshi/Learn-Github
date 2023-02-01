46d14bf54fad2701cb85c1537471e168f73ca54d
SanketSuryawanshi <suryawanshisanket69@gmail.com>
2023-02-01 10:18:50 +0530

    Division File

--- main3.py
+++ main3.py
@@ -1,4 +1,5 @@
 a = int(input("Enter 1st Number :"))
 b = int(input("Enter 2nd Number :"))
 
-print(f'The Multiplication is : {a*b}')
+print(f'The Multiplication is : {a*b}') 
+print(f'The Division is : {a/b}') 
